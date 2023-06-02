ddf561c2a2bd5d91408d0fedb8bff7ae5bbbfa08
주진표 <68942200+jinpyojoo@users.noreply.github.com>
2023-06-02 19:52:30 +0900

    Add files via upload

--- player.js
+++ player.js
@@ -6,49 +6,72 @@ const Application = function () {
 translate = {
     'A': 9,'A♯': 10,'B': 11,'C': 0,'C♯': 1,'D': 2,'D♯': 3,'E': 4,'F': 5,'F♯': 6,'G': 7,'G♯': 8
 }
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var player;
 
+played = false
 
+setTimeout(()=>{
+    document.getElementById('loading').style.opacity = "0"
+    document.getElementById('main-menu').style.display = "block"
+}, 2000)
+
+function run(){
+    document.getElementById('ready-menu').style.display = "none"
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var player;
+}
+url = ""
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
-    videoId: 'YZ6Adbmbpgc',  controls: '0', events: {
+    videoId: url,     playerVars: {'controls': 0 }, events: {
       'onReady': onPlayerReady,    'onStateChange': onPlayerStateChange
     }
   });
 }
 function onPlayerReady(event) {
+    player.playVideo()
+    app.start()
+}
+
+load_music = (data)=>{
+    try{
+        data = JSON.parse(data)
+        url = data['id']
+        tempo = data['tempo']
+        mel = []
+        tem = []
+        Object.keys(tempo).forEach((timestamp)=>{
+            mel.push(tempo[timestamp])
+            tem.push(timestamp)
+        })
+        document.getElementById('main-menu').style.display = "none"
+        document.getElementById('ready-menu').style.display = "block"
+        document.addEventListener('click',()=>{
+            if (!played){
+                played = true
+                run()
+            }
+        })
+    } catch(e) {
+        alert("손상된 KARA파일 입니다.")
+    }
+    
 }
 
 function onPlayerStateChange(event){
     if (event.data == 1){
         if (!trustInteraction){
             document.getElementById('barrier').style.display = "block"
-            fetch('/nightdancer.json')
-            .then((Response)=>Response.json())
-            .then((data)=>{
-                tempo = data
-                mel = []
-                tem = []
-                Object.keys(tempo).forEach((timestamp)=>{
-                    mel.push(tempo[timestamp])
-                    tem.push(timestamp)
-                })
-            })
-            player.playVideo()
-            app.start()
         }
         trustInteraction = true
     }
     if (event.data == 0){
         musicStop = true
-        document.getElementById('mel_status').style.display = "none"
         document.getElementById('barrier').style.background = "black"
         document.getElementById('barrier').style.opacity = "1"
-        document.getElementById('status').innerHTML = "정확한 음 : "+success.length+"/"+mel.length+"<br>부정확한 음 : "+(mel.length - success.length)+"/"+mel.length+"<br>타이밍 놓친 음 : "+slient.length
+        document.getElementById('status').innerHTML = "정확한 음 : "+success.length+"/"+mel.length+"<br>부정확한 음 : "+(mel.length - success.length)+"/"+mel.length+"<br>타이밍 놓친 음 : "+slient.length+"<br><p onclick='location.reload()'>다시 고르기</p>"
     }
 }
 
@@ -85,9 +108,7 @@ tempo = {}
 mel = []
 
   Application.prototype.update = function (note) {
-    
-    document.getElementById('mel_status').innerHTML = "당신 : " + note['name']+"<br>멜로디 : " + mel[step]
-    if (Math.abs(translate[mel[step]] - translate[note['name']]) <= 2){
+        if (Math.abs(translate[mel[step]] - translate[note['name']]) <= 2){
         
         if (success.indexOf(step) == -1){
             document.getElementById('status').innerHTML = "O"
@@ -96,7 +117,11 @@ mel = []
         }
     } else {
         if (failure.indexOf(step) == -1){
-            document.getElementById('status').innerHTML = "X"
+            if (translate[mel[step]] < translate[note['name']]){
+                document.getElementById('status').innerHTML = "HIGH"
+            } else {
+                document.getElementById('status').innerHTML = "LOW"
+            }
             document.getElementById('status').className = "fail_animation"
             failure.push(step)
 
