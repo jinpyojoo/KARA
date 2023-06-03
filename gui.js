9ad395c11b8d32a297df5d55475ef98c7329713c
주진표 <68942200+jinpyojoo@users.noreply.github.com>
2023-06-03 15:13:01 +0900

    kkk

--- gui.js
+++ gui.js
@@ -1,11 +1,11 @@
-pick = async () => {
+pick =  async ()=>{
     let folder = document.getElementById('input-box').value;
     let file_div = document.getElementById('file-name');
     let random_filename = await eel.pick_file(folder)();
 }
 
-fileParser = () => {
-    if (file) {
+fileParser = ()=>{
+    if (file){
         var reader = new FileReader();
         reader.readAsText(file, "UTF-8");
         reader.onload = function (evt) {
@@ -20,7 +20,7 @@ fileParser = () => {
 }
 
 ms = document.getElementById('music_select')
-ms.addEventListener('change', (e) => {
+ms.addEventListener('change', (e)=>{
     file = e.target.files[0]
     fileParser(file)
 })
